a0c819daab6bc064c1a63dcc6849a6799e95d58e
haleyap <122311183+haleyap@users.noreply.github.com>
2025-03-03 20:24:28 -0500

    Update identity.ts
    naming AJ

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Haley`;
 export const OWNER_DESCRIPTION: string = `UNC Student`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `AJ`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
