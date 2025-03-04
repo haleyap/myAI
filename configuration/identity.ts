ed11ee77728db418edf10eb068c1cbd5a15eb3a0
haleyap <122311183+haleyap@users.noreply.github.com>
2025-03-03 20:16:16 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Haley`;
+export const OWNER_DESCRIPTION: string = `UNC Student`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
