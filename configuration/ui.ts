fdbfed4ae6afd16e69bc6456ca4549e41a5c505e
haleyap <122311183+haleyap@users.noreply.github.com>
2025-03-03 21:29:54 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `Ask me a question!`;
 export const FOOTER_MESSAGE: string = `Customized AI by Haley Parker at UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
 export const PAGE_TITLE: string = `AJ!`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s ECON 425 AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
