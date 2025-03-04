6663747a667691c9b80883ec001bdf71e7484ab5
haleyap <122311183+haleyap@users.noreply.github.com>
2025-03-04 15:39:17 -0500

    Update ui.ts
    renaming page

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `ECON 425 Assistant AJ`;
 export const MESSAGE_PLACEHOLDER: string = `Ask me a question!`;
 export const FOOTER_MESSAGE: string = `Customized AI by Haley Parker at UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
-export const PAGE_TITLE: string = `AJ!`;
+export const PAGE_TITLE: string = `ECON 425 AI AJ`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s ECON 425 AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
