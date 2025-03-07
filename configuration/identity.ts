42ba491c2609632d1b2c037852b8974959d06d65
haleyap <122311183+haleyap@users.noreply.github.com>
2025-03-07 15:07:59 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Haley`;
 export const OWNER_DESCRIPTION: string = `UNC Student`;
 
 export const AI_NAME: string = `AJ`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Engaging, Knowledgeable, and Instructive`;
+export const AI_ROLE: string = `ECON 425: Financial Economics Study Assistant and Learning Companion`;
